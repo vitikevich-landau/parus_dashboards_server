d4698919725cd88119fd9e700d698c397affec56
Viktor <60886359+vitikevich-landau@users.noreply.github.com>
2021-09-20 17:06:55 +0700

    # Change delay time

--- middlewares/delay.js
+++ middlewares/delay.js
@@ -1,7 +1,7 @@
 const delay = async (req, res, next) => {
     const randomInteger = (min, max) => Math.floor(min + Math.random() * (max + 1 - min));
 
-    await new Promise(resolve => setTimeout(resolve, randomInteger(250, 950)));
+    await new Promise(resolve => setTimeout(resolve, randomInteger(450, 1250)));
 
     next();
 };
